23c39272ba37f4039c0f305040d977f157e289c8
Tero Niemi <tero.niemi@insta.fi>
2023-09-28 23:26:13 +0300

    Documenting

--- promulgator/setup.py
+++ promulgator/setup.py
@@ -24,7 +24,7 @@ def get_content_files_to_be_published(paths: list[Path]) -> list[dict]:
         content_dir, content_file = _split_to_dir_and_filename(path)
         root_dir, root_config = _seek_content_root(content_dir)
 
-        if content_file is None:  # we have a directory:
+        if content_file is None:  # We have a directory:
             content_files.update(
                 _seek_content_files(content_dir, (root_dir, root_config))
             )
@@ -34,29 +34,31 @@ def get_content_files_to_be_published(paths: list[Path]) -> list[dict]:
     return _trim_paths_and_expand_configs(content_files)
 
 
-def _split_to_dir_and_filename(content: Path) -> tuple[DirName, FileName]:
+def _split_to_dir_and_filename(path: Path) -> tuple[DirName, FileName]:
     """### Split user provided path into absolute dir and a filename
 
-    If path is a directory, then the filename is `None`.
+    Returns tuple: `(content_dir, content_file)`
+
+    If path is a directory, then `content_file` is `None`.
 
     Throws `FileSeekingError` if file is not a markdown file,
     or dir is not found.
     """
-    content = os.path.abspath(content)
+    path = os.path.abspath(path)
 
-    if os.path.isfile(content):
-        content_dir, content_file = os.path.split(content)
+    if os.path.isfile(path):
+        content_dir, content_file = os.path.split(path)
 
         # Sanity check:
         if not CONTENT_FILE_RE.search(content_file):
-            raise FileSeekingError(f"{content} is not a markdown file")
+            raise FileSeekingError(f"{path} is not a markdown file")
 
-    elif os.path.isdir(content):
-        content_dir = content
+    elif os.path.isdir(path):
+        content_dir = path
         content_file = None
 
     else:
-        raise FileSeekingError(f"{content} is not a file or directory")
+        raise FileSeekingError(f"{path} is not a file or directory")
 
     return (content_dir, content_file)
 
@@ -64,8 +66,10 @@ def _split_to_dir_and_filename(content: Path) -> tuple[DirName, FileName]:
 def _seek_content_root(content_dir: DirName) -> tuple[DirName, Config]:
     """### Try to find the content root directory and read the configuration
 
-    Seek nearest parent directory that has a `.promulgator[.y[a]ml]` file.
-    Read it, and return the `root_dir` and `root_config`.
+    Seeks the nearest parent directory that has a `.promulgator[.y[a]ml]` file.
+    After finding it, reads `root_config` from it.
+
+    Returns a tuple: `(root_dir, root_config)`
 
     Throws `FileSeekingError` if no parent directory is found.
     """
@@ -84,7 +88,7 @@ def _seek_content_root(content_dir: DirName) -> tuple[DirName, Config]:
             break
 
     raise FileSeekingError(
-        ".promulgator file was not found in " f"the parent directories of {content_dir}"
+        ".promulgator file was not found in the parent directories of {content_dir}"
     )
 
 
@@ -92,7 +96,8 @@ def _seek_content_files(
     content_dir: DirName, payload: any
 ) -> list[tuple[DirName, FileName]]:
     """### Seek all content files from a directory and all of its subdirectories
-    Returns a dictionary: `(content_dir, content_file) -> payload`
+
+    Returns a dictionary of files: `(content_dir, content_file) -> payload`
     """
     content_files = {}
 
@@ -124,21 +129,24 @@ def _trim_paths_and_expand_configs(
     }
     ```
     """
-    config_cache = {}  # absolute_dir -> configuration
+    config_cache = {}  # absolute_dir -> full_configuration
 
     def get_config(absolute_dir, root_dir, root_config):
         """Return full configuration for `absolute_dir`."""
         nonlocal config_cache
 
+        # If config can be found at cache, use it
         if absolute_dir in config_cache:
             return config_cache[absolute_dir]
 
+        # Read (possible) configuration file in this directory
         config = {}
         for file in os.listdir(absolute_dir):
             if METADATA_FILE_RE.search(file):
                 with open(os.path.join(absolute_dir, file)) as stream:
                     config = yaml.safe_load(stream)
 
+        # Get parent directory configuration
         if absolute_dir == root_dir:
             parent_config = root_config
         else:
@@ -146,6 +154,7 @@ def _trim_paths_and_expand_configs(
                 os.path.split(absolute_dir)[0], root_dir, root_config
             )
 
+        # Merge and push to the cache
         config_cache[absolute_dir] = parent_config | config
         return config_cache[absolute_dir]
 
